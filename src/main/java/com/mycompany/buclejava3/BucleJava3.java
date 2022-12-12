9113b9f74bd23b3f3c8e78f94d25af6ae24898fa
a22hugorp <a22hugorp@192.168.56.1>
2022-12-12 11:30:28 +0100

    java3

---
+++ src/main/java/com/mycompany/buclejava3/BucleJava3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.buclejava3;
+
+/**
+ *
+ * @author a22hugorp
+ */
+public class BucleJava3 {
+
+    public static void main(String[] args) {
+       
+       
+        for ( int i=10; i>=0; i-- ) {
+            System.out.println("Eu son a interacción " + i);
+        }
+    }
+}
